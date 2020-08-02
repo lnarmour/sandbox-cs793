7ee09a161402ea417179c79d800f23122166a327
Louis Narmour <lnarmour@rams.colostate.edu>
2020-08-02 14:23:12 -0600

    asdf

--- alphaz/chiranjeb/2/bpmax.cs
+++ alphaz/chiranjeb/2/bpmax.cs
@@ -3,11 +3,12 @@ system = "bpmax";
 
 outDir = "out";
 
-setSpaceTimeMap(prog, system, "FMIN", "(i,j,k -> 0,i,k,j)", "(i,j -> 0,i,0,j)");
-setSpaceTimeMap(prog, system, "FMAX", "(i,j,k -> 0,i,k,j)", "(i,j -> 0,i,0,j)");
-setSpaceTimeMap(prog, system, "DMIN", "(i,j,k -> 0,i,k,j)", "(i,j -> 0,i,0,j)");
-setSpaceTimeMap(prog, system, "DMAX", "(i,j,k -> 0,i,k,j)", "(i,j -> 0,i,0,j)");
+setSpaceTimeMap(prog, system, "FMIN", "(i,j -> 0,i,j)");
+setSpaceTimeMap(prog, system, "FMAX", "(i,j -> 0,i,j)");
+setSpaceTimeMap(prog, system, "DMIN", "(i,j -> 0,i,j)");
+setSpaceTimeMap(prog, system, "DMAX", "(i,j -> 0,i,j)");
 
 AShow(prog);
 
-generateScheduledCode(prog, system, outDir);
+#generateScheduledCode(prog, system, outDir);
+generateWriteC(prog, system, outDir);
